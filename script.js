63c2f371f115bad6bd890ac7625ceaf4b2b5193a
Edirsong <123476716+Edirsong@users.noreply.github.com>
2024-01-20 19:53:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
     $('.form_contact').submit(function(event){
-        event.preventDefault(); // Prevenir el envío tradicional del formulario
+        event.preventDefault(); // Previene el envío tradicional del formulario
+
+        var $btnSend = $('#btnSend');
+        $btnSend.prop('disabled', true).text('Enviando...'); // Deshabilita el botón y cambia el texto
 
         if (this.checkValidity() === true) {
             var formData = {
@@ -11,27 +14,23 @@ $(document).ready(function(){
                 'mensaje': $('#mensaje').val()
             };
 
-            // Deshabilitar el botón de envío
-            $('#btnSend').attr('disabled', true).text('Enviando...');
-
-            // Llamada AJAX para enviar los datos del formulario
             $.ajax({
                 url: 'https://script.google.com/macros/s/AKfycbxGDbp3aLtGq2Kc76vyZveKGBDehfLYkYAzyq7-cFZP9kRrsl7m4h1wI_W5qLpJx7A/exec',
                 type: 'POST',
                 data: formData,
                 success: function(response) {
                     alert("Mensaje enviado con éxito.");
-                    // Limpiar el formulario
-                    $('.form_contact')[0].reset();
-                    // Habilitar el botón y restablecer su texto
-                    $('#btnSend').attr('disabled', false).text('Enviar Mensaje');
+                    $('.form_contact')[0].reset(); // Limpia el formulario
+                    $btnSend.prop('disabled', false).text('Enviar Mensaje'); // Habilita el botón y restablece el texto
                 },
                 error: function(response) {
                     alert("Hubo un error al enviar el mensaje.");
-                    // Habilitar el botón y restablecer su texto
-                    $('#btnSend').attr('disabled', false).text('Enviar Mensaje');
+                    $btnSend.prop('disabled', false).text('Enviar Mensaje'); // Habilita el botón y restablece el texto
                 }
             });
+        } else {
+            // Si el formulario no es válido, habilitamos de nuevo el botón
+            $btnSend.prop('disabled', false).text('Enviar Mensaje');
         }
     });
 });
